bcc30967a94155144d4c1230bf6f6405ad1ef0d4
RW0990 <whiteryan2599@icloud.com>
2026-08-04 14:03:31 +0100

    Update show dates for events in eventData.js

--- models/eventData.js
+++ models/eventData.js
@@ -10,7 +10,7 @@ async function Connect() {
   await shows.insertMany([
     {
       showTitle: "Rock Party 2026 Live",
-      showDate: new Date("2026-08-24"),
+      showDate: new Date("2026-08-14"),
       venue: "3Arena",
       location: "Dublin, Ireland",
       description: "The Greatest Rock Bands join together for one night!",
@@ -18,7 +18,7 @@ async function Connect() {
     },
     {
       showTitle: "Electric Picnic 2026",
-      showDate: new Date("2026-8-31"),
+      showDate: new Date("2026-8-20"),
       venue: "Kilmainham",
       location: "Dublin, Ireland",
       description: "The biggest music music festival in Ireland!",
@@ -30,6 +30,22 @@ async function Connect() {
   await event.deleteMany({});
   await event.insertMany([
     {
+      showTitle: "Rock Party 2026 Live",
+      showDate: new Date("2026-08-14"),
+      venue: "3Arena",
+      location: "Dublin, Ireland",
+      description: "The Greatest Rock Bands join together for one night!",
+      price: 100,
+    },
+    {
+      showTitle: "Electric Picnic 2026",
+      showDate: new Date("2026-8-20"),
+      venue: "Kilmainham",
+      location: "Dublin, Ireland",
+      description: "The biggest music music festival in Ireland!",
+      price: 50,
+    },
+    {
       showTitle: "Meet and greet with the band",
       showDate: new Date("2026-09-24"),
       venue: "Gibson Hotel",
@@ -39,7 +55,7 @@ async function Connect() {
     },
     {
       showTitle: "Acoustic Gig",
-      showDate: new Date("2026-8-31"),
+      showDate: new Date("2026-9-31"),
       venue: "Whelan's",
       location: "Dublin, Ireland",
       description: "Small acoustic session for our biggest fans!",
