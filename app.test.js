1a161f2d204f9446fed1e4ddfb7b1abc957e1cf1
RW0990 <whiteryan2599@icloud.com>
2026-08-06 16:04:33 +0100

    Add files via upload

--- app.test.js
+++ app.test.js
@@ -1,50 +1,76 @@
-import { describe, test, it, expect, vi } from "vitest";
+import {
+  describe,
+  test,
+  it,
+  expect,
+  vi,
+  beforeAll,
+  afterAll,
+  beforeEach,
+} from "vitest";
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "./app.js";
+import User from "./models/User.js";
+
+const dburi =
+  "mongodb+srv://whiteryan2599_db_user:rirjo6-mawpiw-dawTep@test.qmwnz42.mongodb.net/Test";
 
 describe("get/merchandise", () => {
-  it("should return merchandise page", () => {
+  it("should return merchandise page", async () => {
     /*variables*/
-    const response = {
-      render: vi.fn(),
-    };
-    /*method*/
-    ((request, response) => {
-      response.render("merchandise", { title: "Merchandise" });
-    })({}, response);
-    /*result expected*/
-    expect(response.render).toHaveBeenCalledWith("merchandise", {
-      title: "Merchandise",
-    });
+    const response = await request(app).get("/merchandise");
+    expect(response.status).toBe(200);
+    expect(response.text).toContain("Merchandise");
   });
 });
+
 describe("get/dashboard", () => {
-  it("should return dashboard page", () => {
+  it("should return dashboard page", async () => {
     /*variables*/
-    const response = {
-      render: vi.fn(),
-    };
-    /*method*/
-    ((request, response) => {
-      response.render("dashboard", { title: "Dashboard" });
-    })({}, response);
-    /*result expected*/
-    expect(response.render).toHaveBeenCalledWith("dashboard", {
-      title: "Dashboard",
-    });
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toContain("Dashboard");
   });
 });
+
 describe("get/cart", () => {
-  it("should return cart page", () => {
+  it("should return cart page", async () => {
     /*variables*/
-    const response = {
-      render: vi.fn(),
-    };
-    /*method*/
-    ((request, response) => {
-      response.render("cart", { title: "Cart" });
-    })({}, response);
-    /*result expected*/
-    expect(response.render).toHaveBeenCalledWith("cart", {
-      title: "Cart",
-    });
+    const response = await request(app).get("/cart");
+    expect(response.status).toBe(200);
+    expect(response.text).toContain("Cart");
+  });
+});
+
+beforeAll(async () => {
+  //connection to our test cluster on mongodb
+  await mongoose.connect(dburi);
+});
+beforeEach(async () => {
+  //delete any data and start with fresh usernames and passwords
+  await User.deleteMany({});
+  await User.create({ email: "test@email.com", password: "Newpassword123" });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+describe("POST /login", () => {
+  it("allows user to log in with valid details only", async () => {
+    const response = await request(app)
+      .post("/login")
+      .type("form")
+      .send({ email: "test@email.com", password: "Newpassword123" });
+    expect(response.status).toBe(302);
+    expect(response.headers.location).toBe("/?login=true");
+  });
+  it("blocks sigin in due to invalid details", async () => {
+    const response = await request(app)
+      .post("/login")
+      .type("form")
+      .send({ email: "test@email.com", password: "wrong123" });
+    expect(response.status).toBe(200);
+    expect(response.text).toContain("Email or password incorrect");
   });
 });
